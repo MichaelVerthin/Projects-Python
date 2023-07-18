8241ced59aa6a8fd4e981db7c6652d6facd9b53d
MichaelVerthin <michaelvn5202@gmail.com>
2023-07-18 13:30:08 +0300

    added stocks helper

--- Barista.py
+++ Barista.py
@@ -1,6 +1,7 @@
 # This is a trinning program
 print("Welcome to Michael's coffee shop")
 name = input("\nWhat is your name?\n")
+clear()
 manu = "\n1. Coffee\n2. Breakfest\n3. sweats"
 order = input(print("Hello, " + name + ", What do you want to order today?" + manu))
 print("Sounds great " + name + ", we'll make have that " + order + " ready for you in a moment.")
---
+++ Stock.py
@@ -0,0 +1,21 @@
+import os
+CorUsd = 3.56
+dealCost = 6
+def calcplz():
+    clear = lambda: os.system('cls')
+    print("Welcome to the Stock Calculator!")
+    print("Buy?\n")
+    sumBuy = float(input(">> "))
+    clear()
+    print("Amount?\n")
+    Amount = float(input(">> "))
+    clear()
+    print("Current price?\n")
+    sumSell = float(input(">> "))
+    clear()
+    Cost = ((((sumSell-sumBuy)*Amount)-dealCost)*CorUsd)*0.75
+    print("If you Sell it now, you will get "+ str(Cost))
+    input()
+    calcplz()
+calcplz()
+input()
