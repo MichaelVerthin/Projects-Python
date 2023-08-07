50391eab4c6e5470f1cfbb152f1f9d91a5d8c2ca
MichaelVerthin <michaelvn5202@gmail.com>
2023-08-07 19:53:48 +0300

    test fix for branch

--- Barista.py
+++ Barista.py
@@ -3,6 +3,7 @@
 print("Welcome to Michael's coffee shop")
 name = input("\nWhat is your name?\n")
 clear()
+
 manu = "\n1. Coffee\n2. Breakfest\n3. sweats"
 order = input(print("Hello, " + name + ", What do you want to order today?" + manu))
 print("Sounds great " + name + ", we'll make have that " + order + " ready for you in a moment.")
